193e8a239541c707eedda3390181d46060b126c1
eminebgmnc <eminebgmnc19@gmail.com>
2021-01-20 23:53:11 +0300

    4.gün

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>/*T ne türde bilgi tutacağımız bu herşey olabilir int string vs*/
+    {
+       T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
